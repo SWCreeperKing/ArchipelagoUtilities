843eec2de82709c9a89b1b8c3916e4caa93e0d1e
Alex Gilbert <alexgilbert@yahoo.com>
2024-07-25 17:01:51 -0400

    - Add harmony prefix constants

---
+++ KaitoKid.ArchipelagoUtilities.Net/KaitoKid.ArchipelagoUtilities.Net/Constants/Prefix.cs
@@ -0,0 +1,14 @@
+﻿namespace KaitoKid.ArchipelagoUtilities.Net.Constants
+{
+    public static class Prefix
+    {
+        public const bool RUN_ORIGINAL = true;
+        public const bool DONT_RUN_ORIGINAL = false;
+
+        public const bool RUN_ORIGINAL_LOGIC = RUN_ORIGINAL;
+        public const bool DONT_RUN_ORIGINAL_LOGIC = DONT_RUN_ORIGINAL;
+
+        public const bool RUN_ORIGINAL_METHOD = RUN_ORIGINAL;
+        public const bool DONT_RUN_ORIGINAL_METHOD = DONT_RUN_ORIGINAL;
+    }
+}
