26ea7a9f10fce20599e235cdd6377f07976d7050
Alex Gilbert <alexgilbert@yahoo.com>
2024-07-26 00:57:53 -0400

    - Added the FireAndForget utility

---
+++ KaitoKid.ArchipelagoUtilities.Net/KaitoKid.ArchipelagoUtilities.Net/Extensions/TaskExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using KaitoKid.ArchipelagoUtilities.Net.Interfaces;
+
+namespace DLCQuestipelago.Extensions
+{
+    public static class TaskExtensions
+    {
+        private static ILogger _logger;
+
+        public static void Initialize(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public static async void FireAndForget(this Task task)
+        {
+            try
+            {
+                await task;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Exception occurred in FireAndForget task: {ex}");
+            }
+        }
+    }
+}
