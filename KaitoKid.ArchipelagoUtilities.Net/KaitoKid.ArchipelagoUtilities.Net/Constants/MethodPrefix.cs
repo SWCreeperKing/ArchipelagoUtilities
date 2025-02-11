1c053544336c7cbcf890b6df009c3739711998ac
Alex Gilbert <alexgilbert@yahoo.com>
2025-02-11 09:57:27 -0500

    - Remove redundant constants

--- KaitoKid.ArchipelagoUtilities.Net/KaitoKid.ArchipelagoUtilities.Net/Constants/MethodPrefix.cs
+++ KaitoKid.ArchipelagoUtilities.Net/KaitoKid.ArchipelagoUtilities.Net/Constants/MethodPrefix.cs
@@ -2,13 +2,7 @@
 {
     public static class MethodPrefix
     {
-        public const bool RUN_ORIGINAL = true;
-        public const bool DONT_RUN_ORIGINAL = false;
-
-        public const bool RUN_ORIGINAL_LOGIC = RUN_ORIGINAL;
-        public const bool DONT_RUN_ORIGINAL_LOGIC = DONT_RUN_ORIGINAL;
-
-        public const bool RUN_ORIGINAL_METHOD = RUN_ORIGINAL;
-        public const bool DONT_RUN_ORIGINAL_METHOD = DONT_RUN_ORIGINAL;
+        public const bool RUN_ORIGINAL_METHOD = true;
+        public const bool DONT_RUN_ORIGINAL_METHOD = false;
     }
 }
