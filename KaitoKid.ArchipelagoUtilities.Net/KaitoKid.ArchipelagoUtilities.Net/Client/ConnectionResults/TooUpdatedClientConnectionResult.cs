b54d1254103245dc728edd87b8745a4c7ae2fb9f
Alex Gilbert <alexgilbert@yahoo.com>
2025-07-10 13:24:35 -0400

    - Fixed an error message

--- KaitoKid.ArchipelagoUtilities.Net/KaitoKid.ArchipelagoUtilities.Net/Client/ConnectionResults/TooUpdatedClientConnectionResult.cs
+++ KaitoKid.ArchipelagoUtilities.Net/KaitoKid.ArchipelagoUtilities.Net/Client/ConnectionResults/TooUpdatedClientConnectionResult.cs
@@ -9,11 +9,11 @@ namespace KaitoKid.ArchipelagoUtilities.Net.Client.ConnectionResults
     internal class TooUpdatedClientConnectionResult : ClientVersionMismatchConnectionResult
     {
         public TooUpdatedClientConnectionResult(string clientName, string currentVersion, string expectedVersion) :
-            this($"This Multiworld has been created for {clientName} version {expectedVersion},{Environment.NewLine}but this is {clientName} version {currentVersion}.{Environment.NewLine}This can be caused by accidentally installing a pre-release mod.{Environment.NewLine}Please downgrade to a compatible mod version.", clientName, currentVersion, expectedVersion)
+            this($"This Multiworld has been created for {clientName} version {expectedVersion}, but this is {clientName} version {currentVersion}.{Environment.NewLine}This can be caused by accidentally installing a pre-release mod.{Environment.NewLine}Please downgrade to a compatible mod version.", clientName, currentVersion, expectedVersion)
         {
         }
 
-        public TooUpdatedClientConnectionResult(string errorMessage, string clientName, string currentVersion, string expectedVersion) : base(errorMessage, currentVersion, expectedVersion)
+        public TooUpdatedClientConnectionResult(string errorMessage, string clientName, string currentVersion, string expectedVersion) : base(errorMessage, clientName, currentVersion, expectedVersion)
         {
         }
     }
